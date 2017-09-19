91e56841160373b6d171cb37d8248dc9c93b1c18
Naren Iyer <naren.iyer@hotmail.com>
2017-09-19 20:17:55 +0100

    Mongodb enabled and running

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -13,6 +13,17 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
+describe port(80) do
   it { should_not be_listening }
 end
+
+describe package 'mongodb' do
+	it { should be_installed }
+end
+
+describe service 'mongodb' do 
+	it {should be_running }
+	it {should be_enabled }
+end
+
+
