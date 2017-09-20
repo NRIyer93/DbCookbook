a0056ce4595d89551ee1f44026638faadb20685f
Naren Iyer <naren.iyer@hotmail.com>
2017-09-20 10:42:22 +0100

    Mongodb-org added to default file

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+apt_repository 'mongodb-org' do 
+  uri "http://repo.mongodb.org/apt/ubuntu"
+  distribution "xenial/mongodb-org/3.2"
+  components ["multiverse"]
+  keyserver "hkp://keyserver.ubuntu.com:80"
+  key "EA312927"
+end
+
 package 'mongodb'
 
 service 'mongodb' do
-	supports status: true, restart: true, reload: true
+	supports status: true, restart: true
 	action [:enable, :start]
 end
 
@@ -17,5 +25,4 @@ end
 
  template '/lib/systemd/system/mongod.service' do
  	source 'mongod.service.erb'
- 	notifies :reload, "service[mongodb]"
  end
